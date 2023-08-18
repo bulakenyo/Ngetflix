de808cd633467352958c69fc2639ffbe9e8ac3cc
henryandesjr <henryandesjr@gmail.com>
2023-08-19 02:17:20 +0800

    Initial commit for Ngetflix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeatureComponent } from './feature/feature.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { MovieComponent } from './movie/movie.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeatureComponent,
+    HeaderComponent,
+    HomeComponent,
+    LoginComponent,
+    MovieComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
